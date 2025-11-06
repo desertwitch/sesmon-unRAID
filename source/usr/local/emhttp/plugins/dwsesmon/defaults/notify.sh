e34a31aea2c7a7ef3a8a245e018b2db304580fee
desertwitch <24509509+desertwitch@users.noreply.github.com>
2025-11-06 08:58:10 +0100

    changes

--- source/usr/local/emhttp/plugins/dwsesmon/defaults/notify.sh
+++ source/usr/local/emhttp/plugins/dwsesmon/defaults/notify.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 # sesmon notification script
-echo "[$1] [$2] [$3] [$4]" | logger -t "sesmon-notify"
+
+SES_DEV_PATH="$1"
+SES_DEV_ADDR="$2"
+SES_DEV_DESCR="$3"
+SES_ALARM_MSG="$4"
+SES_ALARM_JSON="$5"
+
+NOTIFY="/usr/local/emhttp/plugins/dynamix/scripts/notify"
+HOST="$(echo "$HOSTNAME" | awk '{print toupper($0)}')"
+EVENT="SCSI Enclosure Alert"
+SUBJECT="[${HOST}] SES: [${SES_DEV_PATH}:${SES_DEV_ADDR}] ${SES_DEV_DESCR}"
+
+"$NOTIFY" -e "${EVENT}" -s "Alert ${SUBJECT}" -d "${SES_ALARM_MSG}" -i "alert"
