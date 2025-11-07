b28fe3ef35530b42d012a87d3cc881ef8462b393
desertwitch <24509509+desertwitch@users.noreply.github.com>
2025-11-07 11:10:44 +0100

    changes

--- source/usr/local/emhttp/plugins/dwsesmon/defaults/notify.sh
+++ source/usr/local/emhttp/plugins/dwsesmon/defaults/notify.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
+# shellcheck disable=SC2034
+#
 # sesmon notification script
+#
+# Do not change this unless you know what you are doing.
+# Advanced filtering is possible e.g. with "jq" using "SES_ALARM_JSON".
+# You can take a look at the JSON files in "/var/lib/sesmon" for reference.
+#
+SES_DEV_PATH="$1" # device path, e.g. /dev/sg/25
+SES_DEV_ADDR="$2" # device SAS address, e.g. 0x500a098012345678
+SES_DEV_DESCR="$3" # device description, e.g. "My JBOD"
+SES_ALARM_MSG="$4" # alarm message (textual representation)
+SES_ALARM_JSON="$5" # alarm JSON (see JSON files for reference)
 
-SES_DEV_PATH="$1"
-SES_DEV_ADDR="$2"
-SES_DEV_DESCR="$3"
-SES_ALARM_MSG="${4//=/\:}" # for Unraid 7.1.x
-SES_ALARM_MSG="${SES_ALARM_MSG//#/\:}" # for Unraid 7.2.x
-SES_ALARM_JSON="$5"
+# Unraid's notification system doesn't like some special characters,
+# so we need to replace them for the notification system not to break:
+SES_ALARM_MSG="${SES_ALARM_MSG//=/:}" # bugfix for Unraid 7.1.x
+SES_ALARM_MSG="${SES_ALARM_MSG//#/:}" # bugfix for Unraid 7.2.x
 
 NOTIFY="/usr/local/emhttp/plugins/dynamix/scripts/notify"
 HOST="$(echo "$HOSTNAME" | awk '{print toupper($0)}')"
 EVENT="SCSI Enclosure Alert"
 SUBJECT="[${HOST}] SES: [${SES_DEV_PATH}:${SES_DEV_ADDR}] ${SES_DEV_DESCR}"
 
+# Dispatch the notification through the OS notification system:
 "$NOTIFY" -e "${EVENT}" -s "Alert ${SUBJECT}" -d "${SES_ALARM_MSG}" -i "alert"
